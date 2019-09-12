1fd832f541f7da89c4b8c41d4cfa099dd663cec8
foostart.com <foostart.com@gmail.com>
2019-09-12 09:10:53 +0700

    cm

---
+++ 914/914-content.php
@@ -0,0 +1,91 @@
+﻿<?php
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+?>
+        <div class="type-1317">
+
+
+            <div class="content_wrap-2">
+                <div class="container">
+                    <div class="wrapper">
+                        <div class="row">
+                            <div class="breadcrumbs">
+                                <a class="breadcrumbs_item home" href="#">Home</a>
+                                <span class="breadcrumbs_delimiter">
+
+                                </span><a class="breadcrumbs_item" href="#">Shop</a>
+                                <span class="breadcrumbs_delimiter"></span>
+                                <a class="breadcrumbs_item cat_post" href="#">Racquet Grips</a>
+                                <span class="breadcrumbs_delimiter"></span>
+                                <span class="breadcrumbs_item current">HEAD Instinct Junior 25 Prestrung Tennis Racquet</span>
+                            </div>
+                        </div>    
+                    </div>
+                </div>        
+            </div>
+
+            <div class="wapper2">
+                <div class="container">
+                    <div class="wrapper">
+                        <div class="row">
+                            <div class="col-md-6">
+
+                                <span class="onsale" style="    padding: 12px 12px;    background: red;    color: #eee;    font-weight: bold;">Sale!</span>
+                                <div class="images">
+                                    <a href="images/product-8.jpg" itemprop="image" class="woocommerce-main-image zoom hover_icon hover_icon_view" title="" data-rel="prettyPhoto" rel="magnific">
+                                        <img src="<?php echo $url_path ?>/images/product-8-600x600 (2).jpg" alt=""/></a>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="summary">
+                                    <h1 itemprop="name" class="product_title">HEAD Instinct Junior 25 Prestrung Tennis Racquet</h1>
+                                    <div itemprop="aggregateRating" itemscope="" itemtype="#">
+                                        <i class="fa fa-circle" aria-hidden="true"></i>
+                                        <i class="fa fa-circle" aria-hidden="true"></i>
+                                        <i class="fa fa-circle" aria-hidden="true"></i>
+                                        <i style=" color: #d3ced2;" class="fa fa-circle" aria-hidden="true"></i>
+                                        <i style=" color: #d3ced2;" class="fa fa-circle" aria-hidden="true"></i>
+
+                                        <div itemprop="offers" itemscope="" itemtype="#">
+                                            <p class="price">
+                                                <span class=" amount">
+                                                    <span>£</span>148.00
+                                                </span>
+                                                <span class=" amount1">
+                                                    <del> <span>£</span>199.00</span></del> <ins>
+                                                </ins>
+                                            </p>
+                                            <meta itemprop="price" content="148">
+                                            <meta itemprop="priceCurrency" content="GBP">
+                                            <link itemprop="availability" href="#">
+                                        </div>
+                                        <div class="description">
+                                            <p>Nullam tincidunt tempus ligula, a dapibus massa ullamcorper et. Aliquam facilisis, massa in malesuada vehicula, odio mauris gravida sem, molestie imperdiet metus urna sit amet ex.</p>
+                                        </div>
+                                        <form method="post" enctype="multipart/form-data">
+                                            <div class="quantity">
+                                                <input type="number" step="1" min="1" max="" name="quantity" value="1" title="Qty" class="input-text qty text" size="4" pattern="[0-9]*" inputmode="numeric">
+                                                <span class="q_inc"></span>
+                                                <span ></span>
+                                            </div>
+                                            <input type="hidden" name="add-to-cart" value="1078">
+                                            <button type="submit" class=" alt">Add to cart</button>
+                                        </form>
+
+                                        <span>Categories: <a href="#" rel="tag">Racquet Grips</a>, <a href="#" rel="tag">Racquets</a>, <a href="#" rel="tag">Sales &amp; Deals</a></span></br>
+                                        <span>Tags: <a href="#" rel="tag">equipment</a>, <a href="#/" rel="tag">tennis</a></span></br>
+                                        <span>Product ID: <span>1078</span></span>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>    
+                    </div>
+                </div>
+            </div>
+        </div>
---
+++ 914/914.php
@@ -0,0 +1,50 @@
+﻿<?php
+
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+
+    if (!class_exists('lessc')) {
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
+        require_once($dir_block.'/libs/lessc.inc.php');
+    }
+    
+    $less = new lessc;
+  $less->compileFile('less//914.less', 'css/914.css');   
+?>
+<!DOCTYPE html>
+
+<html lang="en-US">
+
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Infor - Tennis club</title>
+        <meta name="description" content="My Store" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="icon" type="images/x-icon" href="images/favicon.ico" />
+        <script src="<?php echo $url_path ?>/js/jquery-2.1.1.min.js" type="text/javascript"></script>
+        <link href="<?php echo $url_path ?>/framework/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen" />
+        <script src="<?php echo $url_path ?>/framework/bootstrap/js/bootstrap.min.js"></script>
+        <link href="<?php echo $url_path ?>/framework/font-awesome/css/font-awesome.min.css" rel="stylesheet"/>
+        <link href="<?php echo $url_path ?>/framework/fontello/css/fontello.css" rel="stylesheet"/>
+        <link href="<?php echo $url_path ?>/framework/swiper/swiper.css" rel="stylesheet"/>
+        <link href="<?php echo $url_path ?>/framework/shortcodes/theme.shortcodes.css" rel="stylesheet"/>
+        <link href="<?php echo $url_path ?>/framework/owl.carousel.2.0.0-beta.2.4/assets/owl.carousel.css" rel="stylesheet"/>
+        <script type="text/javascript" src="framework/owl.carousel.2.0.0-beta.2.4/owl.carousel.js"></script>
+        <script type="text/javascript" src="framework/swiper/swiper.js"></script>
+        <script type="text/javascript" src="framework/shortcodes/theme.shortcodes.js"></script>
+        <link href="<?php echo $url_path ?>/css/914.css" rel="stylesheet"/>
+    </head>
+
+    <body>
+        <?php include $dir_block.'/914-content.php'; ?>
+
+    </body>
+
+</html>
